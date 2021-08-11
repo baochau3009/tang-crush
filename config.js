75f2722c1a52de7a89f7a0f3b630ba36dfbacbe8
baochau3009 <88767198+baochau3009@users.noreply.github.com>
2021-08-11 14:40:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'hellu pạn!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnIntro: 'hehe',
+    title: 'nếu pạn đang nghĩ zề tui 😙',
+    desc: 'thì đók là lúc mà k pạn bik iu gòi đók ',
+    btnYes: 'Vẫn cứ là thích tui <33',
+    btnNo: 'Không :((((',
+    question:'xao pạn thík tui hay z <3',
+    btnReply: 'Gửi cho bchausinkdep <3',
+    reply: 'vì bchau de thuong hê hê hê <33333333',
+    mess: 'tui biết mà 🥰. Iu pạn nhiều nhiều 😘😘',
+    messDesc: 'Bai đồng chí :3.',
+    btnAccept: 'bai <3',
+    messLink: 'http://fb.com' //profile.php?id=100052187559342
 }
